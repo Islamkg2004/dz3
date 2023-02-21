baf39b3cf56dba2a8c225654ad81e6f7e1ae4297
Islamkg2004 <islam.kg.2004@gmail.com>
2023-02-21 22:25:53 +0600

    dz3_21_01_islam

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            age: ''
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick() {
+        this.setState({name: 'Geeks', age: 15})
+          }
+
+    render() {
+        return (
+            <div>
+                <p>Имя:{this.state.name}, возраст:{this.state.age}</p>
+                <button onClick={this.handleClick}>Нажми на меня</button>
+            </div>
+        )
+    }
+}
+
+
+
+export default App;
